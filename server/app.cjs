4a689ab29f4c1164a7092c9fa2cb978f97d2d427
puissantachille <puissantachille@gmail.com>
2024-05-15 16:34:59 +0000

    cors is fucking terrible

--- server/app.cjs
+++ server/app.cjs
@@ -18,8 +18,10 @@ connection.once('open', () => {
 // app init
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin: 'https://5173-monospace-solstice-1715785461606.cluster-6yqpn75caneccvva7hjo4uejgk.cloudworkstations.dev',
     credentials: true,
+    optionsSuccessStatus: 200,
   })
 );
 
