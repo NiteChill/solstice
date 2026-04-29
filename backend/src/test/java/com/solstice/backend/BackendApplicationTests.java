1dbbb24221545e5d487633285b7a1ef693a6ae9b
NiteChill <puissantachille@gmail.com>
2026-04-29 03:00:02 +0200

    fix: make gradlew create containers for tests

--- backend/src/test/java/com/solstice/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/solstice/backend/BackendApplicationTests.java
@@ -2,12 +2,21 @@ package com.solstice.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BackendApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+			"postgres:17-alpine");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
