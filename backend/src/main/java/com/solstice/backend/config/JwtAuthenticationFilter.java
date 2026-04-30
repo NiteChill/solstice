b10f8ec8042c96684cdc30020216abede69d09c5
NiteChill <puissantachille@gmail.com>
2026-05-01 00:42:03 +0200

    feat(jwt-authentication-filter): catch jwt errors and pass them to global-exception-handler

--- backend/src/main/java/com/solstice/backend/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/solstice/backend/config/JwtAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.solstice.backend.config;
 
+import com.solstice.backend.exception.AccessTokenExpiredException;
 import com.solstice.backend.service.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Component
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
   private final UserDetailsService userDetailsService;
+  private final HandlerExceptionResolver handlerExceptionResolver;
 
   @Override
   protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
@@ -36,24 +40,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       filterChain.doFilter(request, response);
       return;
     }
+    try {
+      jwt = authHeader.substring(7);
+      userEmail = jwtService.extractUsername(jwt);
 
-    jwt = authHeader.substring(7);
-    userEmail = jwtService.extractUsername(jwt);
+      if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
-      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        if (jwtService.isTokenValid(jwt, userDetails)) {
+          UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null,
+                                                                                                  userDetails
+                                                                                                    .getAuthorities());
+          authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-      if (jwtService.isTokenValid(jwt, userDetails)) {
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null,
-                                                                                                userDetails
-                                                                                                  .getAuthorities());
-        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+      }
 
-        SecurityContextHolder.getContext().setAuthentication(authToken);
+      filterChain.doFilter(request, response);
+    } catch (Exception ex) {
+      if (ex instanceof ExpiredJwtException) {
+        handlerExceptionResolver.resolveException(request, response, null,
+                                                  new AccessTokenExpiredException("JWT has expired"));
+      } else {
+        handlerExceptionResolver.resolveException(request, response, null, ex);
       }
     }
-
-    filterChain.doFilter(request, response);
   }
 }
