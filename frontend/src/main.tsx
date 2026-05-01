cae3db75f9a756f78f5711546f790a09e5d1c549
NiteChill <puissantachille@gmail.com>
2026-05-01 21:40:53 +0200

    refactor: update query-client config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,16 @@ import { router } from './router';
 
 import './globals.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Enable when prod.
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
